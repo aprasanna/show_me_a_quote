5224e46fd67bd011d40a279356eeef136523428c
Arvind Prasanna <1108710+aprasanna@users.noreply.github.com>
2018-02-20 01:33:55 -0500

    Limit quote length to 80 characters per line - Also add a space before and after quote

--- show_me_a_quote/quote.py
+++ show_me_a_quote/quote.py
@@ -6,6 +6,7 @@ import json
 import requests
 from datetime import datetime
 import shelve
+import textwrap
 
 colorama_init(autoreset=True)
 
@@ -15,7 +16,8 @@ def quote():
     quotes_api_link = "http://api.forismatic.com/api/1.0/?"
     quotes_api_options = "method=getQuote&lang="+"en"+"&format=json"
     with shelve.open('show_me_a_quote.db') as quote_db:
-        if (('curr_date' not in quote_db) or (quote_db['curr_date'] != curr_date)):
+        print('{}'.format('\n'))
+        if (('curr_date' not in quote_db) or (quote_db['curr_date'] == curr_date)):
             response = requests.get((('{}{}').format(
                 quotes_api_link, quotes_api_options)))
             try:
@@ -30,5 +32,5 @@ def quote():
             quote_db['curr_date'] = curr_date
             quote_db['quote'] = quote
             quote_db['author'] = author
-        print('{}'.format(quote_db['quote']))
-        print('{}'.format(quote_db['author']))
+        print(textwrap.fill(quote_db['quote'], width=80))
+        print('{}{}{}'.format('- ', quote_db['author'], '\n'))
